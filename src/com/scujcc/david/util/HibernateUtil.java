f845bac7c2f830fc918d3340419b29e5b71525ed
David <903205927@qq.com>
2017-04-05 20:58:53 +0800

    modify Hibernateutil.java

--- src/com/scujcc/david/util/HibernateUtil.java
+++ src/com/scujcc/david/util/HibernateUtil.java
@@ -8,12 +8,17 @@ import org.hibernate.cfg.Configuration;
 /**
  * Created by David on 3/29/17.
  */
+
+//该工具类的目的是为那些需要session（会话）的类提供会话工厂。
 public class HibernateUtil {
+
     private static SessionFactory sessionFactory;
 
     static{
         try {
+            //获取hibernate的主配置文件。（注意：只要读了主配置文件，自然就会读其他的配置文件，因为这些配置文件都在主配置文件中定义了的。）
             Configuration configuration = new Configuration().configure();
+
             StandardServiceRegistry serviceRegistry =
                     new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
             sessionFactory = configuration.buildSessionFactory(serviceRegistry);
